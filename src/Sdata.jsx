d5c64929478546fc57798427fa90e88aedb4df15
shakil <shakilalam88@gmail.com>
2022-01-27 14:45:48 +0530

    sdata updated

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,26 +1,32 @@
+import image1 from '../src/image/nike-1.jpg';
+import image2 from '../src/image/nike-2.jpg';
+import image3 from '../src/image/nike-3.jpg';
+import image4 from '../src/image/nike-4.jpg';
+import image5 from '../src/image/nike-5.jpg';
+import image6 from '../src/image/nike-6.jpg';
 const Sdata =[
     {
-        imgsrc:'../Image/nike-1.jpg',
-        title:'Card title 1'
+        imgsrc: image1,
+        title:'Card title 1',
     },
     {
-        imgsrc:'../Image/nike-2.jpg',
+        imgsrc:image2,
         title:'Card title 2'
     },
     {
-        imgsrc:'../Image/nike-3.jpg',
+        imgsrc:image3,
         title:'Card title 3'
     },
     {
-        imgsrc:'../Image/nike-4.jpg',
+        imgsrc:image4,
         title:'Card title 4'
     },
     {
-        imgsrc:'../Image/nike-5.jpg',
+        imgsrc:image5,
         title:'Card title 5'
     },
     {
-        imgsrc:'../Image/nike-6.jpg',
+        imgsrc:image6,
         title:'Card title 6'
     }
 ]
